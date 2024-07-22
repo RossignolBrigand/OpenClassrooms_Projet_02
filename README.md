2b23b2e6d3a1f644f3e2446140d9ad67014d432d
Thomas PINEAU <thomas.pineau93@proton.me>
2024-07-22 19:20:59 +0200

    Added activites and footer  sections

--- README.md
+++ README.md
@@ -2,9 +2,4 @@ Workinprogress branch
 
 TBD
 
-Change infos to info everywhere 
-
 J'ai l'impression qu'il faut créer une div autour du bouton et de l'icone pour pouvoir agir dessus.
-
-Change card text margins and padding 
-Make every card the same size
--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
 // Updates the text based on the search result
 
 let searchResult = 500
-let infoParagraph = document.getElementById("options-infos-text")
+let infoParagraph = document.getElementById("options-info-text")
 let infoText = `Plus de ${searchResult} logements sont disponibles dans cette ville`
 
 infoParagraph.innerHTML = infoText
