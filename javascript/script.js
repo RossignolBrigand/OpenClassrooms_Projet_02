4750332fbacc19dd21b1e91ea37ba6716d3e700f
Thomas PINEAU <thomas.pineau93@proton.me>
2024-08-09 01:03:46 +0200

    last minutes changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,14 @@
 
 // Updates the text based on the search result
 
-let searchResult = 500
-let infoParagraph = document.getElementById("options-info-text")
-let infoText = `Plus de ${searchResult} logements sont disponibles dans cette ville`
+let searchResult = 500;
+let infoParagraph = document.getElementById("options-info-text");
+let infoText = `Plus de ${searchResult} logements sont disponibles dans cette ville`;
+infoParagraph.innerHTML = infoText;
 
-infoParagraph.innerHTML = infoText
+// Filter buttons toggle
+
+document.querySelectorAll('.filter-button').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('active');})
+    });
